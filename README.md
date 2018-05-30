921ae60d4413f32ac8dbbe3dd028953ebd7244f0
Neema Dadmand <ndadmand1990@gmail.com>
2018-05-29 20:23:52 -0700

    first commit
    uploaded first addition of website

---
+++ README.md
@@ -0,0 +1,1 @@
+# ndadmand.github.io
---
+++ main.js
@@ -0,0 +1,7 @@
+// this function is called when page is loaded 
+$( window ).load(function() {
+    
+    
+    
+  $('.introwrapper').parallax({imageSrc: 'pic2.jpg'});
+});
